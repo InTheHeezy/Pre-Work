6cbd83fad4ed05b9710528c6b7a9836f63345d16
Tonny <tonnyan30@gmail.com>
2026-01-04 18:26:42 -0800

    js script test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-fetch("https://www.swapi.tech/api/people/")
+fetch("https://www.swapi.tech/api/planets/1/")
     .then(res => res.json())
-    .then(data => console.log())
+    .then(data => console.log(data))
     .catch(err => console.error(err))
+    
