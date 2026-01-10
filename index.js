83ca96ae06c72b4f7a1de571ea25c92db8d57cd0
Tonny <tonnyan30@gmail.com>
2026-01-09 17:13:51 -0800

    Fetch Film data

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-fetch("https://www.swapi.tech/api/planets/1/")
+
+fetchData();
+
+async function fetchData() {
+
+    try {
+        const response = await fetch("https://www.swapi.tech/api/films");
+
+        if (!response.ok) {
+            throw new Error("Could not fetch");
+        }
+
+        const data = await response.json();
+        const film = data.result;
+
+        console.log(film);
+        //console.log(data.result.properties.name);
+    }
+
+    catch (err) {
+        console.error(err);
+    }
+
+}
+
+
+/*fetch("https://www.swapi.tech/api/people/1/")
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.error(err))
-    
+  */  
