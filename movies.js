4f800917176f0d0c0baa00e19455156ee0da4959
Tonny <tonnyan30@gmail.com>
2026-01-16 15:12:36 -0800

    js creates div

--- movies.js
+++ movies.js
@@ -1,7 +1,7 @@
 showMovies()
 
 async function showMovies() {
-
+    
     try {
         const response = await fetch("https://www.swapi.tech/api/films");
 
@@ -11,14 +11,55 @@ async function showMovies() {
 
         const data = await response.json();
         const film = data.result;
-
+        console.log(film);
         const belowButtons = document.getElementById("belowButtons");
+
+
+        /*
+        Container Test 
+        */
+
+        const containerDiv = document.createElement("container");
+        containerDiv.classList.add("container");
+
+        const imgDiv = document.createElement("image-container");
+        imgDiv.classList.add("image-container");
+        var img = document.createElement("img");
+        img.src = "https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg";
+        imgDiv.appendChild(img);
+
+        const txtDiv = document.createElement("text-container");
+        txtDiv.classList.add("text-container");
+        const titleHeader = document.createElement("h1");
+        titleHeader.textContent = film[0].properties.title;
+
+        const directors = document.createElement("p");
+        directors.textContent = "Director(s): " + film[0].properties.director;
+
+        const producers = document.createElement("p");
+        producers.textContent = "Producer(s): " + film[0].properties.producer;
+
+        const release = document.createElement("p");
+        release.textContent = "Release Date: " + film[0].properties.release_date;
+
+        txtDiv.appendChild(titleHeader);
+        txtDiv.appendChild(directors);
+        txtDiv.appendChild(producers);
+        txtDiv.appendChild(release);
+
+        containerDiv.appendChild(imgDiv);
+        containerDiv.appendChild(txtDiv);
+
+        document.body.appendChild(containerDiv);
+
+        /*
 
         let movieList = "";
 
         for (let i = 0; i < film.length; i++) {
-            belowButtons.innerHTML+=film[i].properties.title + "<br>";
+            belowButtons.innerHTML += film[i].properties.title + "<br>";
         }
+        */
     }
 
     catch (err) {
